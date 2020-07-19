5eb6002274cd1a6720130f6f083cb08c2d83b034
erraX <niminjiecide@outlook.com>
2020-07-19 20:59:20 +0800

    feat(Form,Field): add `Form` and `Field` component

---
+++ src/components/Field.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="form-after-all__field">
+    <slot :field="field" />
+  </div>
+</template>
+
+<script>
+import useField from '../hooks/useField';
+
+const FIELD_DEFAULT_VALUE = undefined;
+
+export default {
+  props: {
+    fieldPath: {
+      type: String,
+      required: true,
+    },
+    initialState: {
+      type: Object,
+      default: () => ({
+        value: FIELD_DEFAULT_VALUE,
+        touched: false,
+        error: false,
+        active: true,
+        editable: true,
+        visible: true,
+      }),
+    },
+    restoreWhenBecomeInactive: {
+      type: Boolean,
+      default: true,
+    },
+    defaultValue: {
+      type: null,
+      default: FIELD_DEFAULT_VALUE,
+    },
+    activeWhen: {
+      type: Function,
+      default: () => true,
+    },
+    visibleWhen: {
+      type: Function,
+      default: () => true,
+    },
+    editableWhen: {
+      type: Function,
+      default: () => true,
+    },
+  },
+
+  setup(props) {
+    const field = useField(props);
+    return { field };
+  },
+};
+</script>
---
+++ src/components/Form.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="form-after-all">
+    <slot :form="form" />
+  </div>
+</template>
+
+<script>
+import { noop } from 'lodash-es';
+import useForm, { useFormProvide } from '../hooks/useForm';
+
+export default {
+  props: {
+    initialState: {
+      type: Object,
+      default: () => ({}),
+    },
+    onReset: {
+      type: Function,
+      default: () => noop,
+    },
+    onSubmit: {
+      type: Function,
+      default: () => noop,
+    },
+    validate: {
+      type: Function,
+      default: () => () => true,
+    },
+    validateOnMount: {
+      type: Boolean,
+      default: false,
+    },
+    validateOnBlur: {
+      type: Boolean,
+      default: true,
+    },
+    validateOnChange: {
+      type: Boolean,
+      default: true,
+    },
+  },
+
+  setup(props) {
+    const form = useForm(props);
+    useFormProvide(form);
+    return { form };
+  },
+};
+</script>

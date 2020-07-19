7f07b9e217220e419c703ee6ed85e5e188d42086
erraX <niminjiecide@outlook.com>
2020-07-19 21:02:03 +0800

    feat(utils): refactor utils

--- src/utils.ts
+++ src/utils.ts
@@ -1,53 +1,79 @@
 import Vue from 'vue';
 import { isRef } from '@vue/composition-api';
 
-// environment is in development or not
+// environment is development or not
 export const isDev: boolean = process.env.NODE_ENv === 'development';
 
-// unwrap `Ref` type value
+/**
+ * unwrap `Ref` type value
+ */
 export const unref = (value: any): any => (isRef(value) ? value.value : value);
 
-// array key is marked as `#1#` `#2#` ...
+/**
+ * array key is marked as `#1#` `#2#` ...
+ * `matchArrayKey` will extract these keys
+ * eg. '#0#' => '0'
+ */
 const REG_ARRAY_KEY = /#(.+)#/;
+const matchArrayKey = (key: string): { matched: boolean; result?: string } => {
+  const result = key.match(REG_ARRAY_KEY);
+  if (!result) {
+    return { matched: false };
+  }
+  return {
+    matched: true,
+    result: result[1],
+  };
+};
 
-// `b[0]`
+/**
+ * match `key[index]` type path
+ * eg. 'a[0]' => `['a', '0']`
+ */
 const REG_ARRAY_PATH = /(.+)\[(.+)]/;
-
-const matchArrayKey = (key: string): string => {
-  const matched = key.match(REG_ARRAY_KEY);
-  if (!matched) {
-    return '';
+const matchArrayPath = (
+  path: string
+): { matched: boolean; result?: [string, string] } => {
+  const result = path.match(REG_ARRAY_PATH);
+  if (!result) {
+    return { matched: false };
   }
-  return matched[1];
+  return {
+    matched: true,
+    result: [result[1], result[2]],
+  };
 };
 
-const matchArrayPath = (path: string) => {
-  const matched = path.match(REG_ARRAY_PATH);
-  if (!matched) {
-    return [];
-  }
-  return [matched[1], matched[2]];
-};
-
-// 'a.b.c' => ['a', 'b', 'c']
-// 'a.b[0].c' => ['a', '#b#', '0', 'c']
-const pathToKeys = (path: string) =>
+/**
+ * split path string to path array
+ * eg.  'a.b.c' => ['a', 'b', 'c']
+ *      'a.b[0].c' => ['a', '#b#', '0', 'c']
+ */
+const pathToKeys = (path: string): string[] =>
   path
     .split('.')
     .filter((p) => p !== '')
     .map((p) => {
-      const arrayPaths = matchArrayPath(p);
-      return !arrayPaths.length ? p : [`#${arrayPaths[0]}#`, arrayPaths[1]];
+      const { matched, result: arrayPath } = matchArrayPath(p);
+      if (!matched || !arrayPath) {
+        return p;
+      }
+      return [`#${arrayPath[0]}#`, arrayPath[1]];
     })
     .flat();
 
-export const $set = (obj: { [key: string]: any }, path: string, value: any) => {
+export const $set = (
+  obj: { [key: string]: any },
+  path: string,
+  value?: any
+) => {
   const keys = pathToKeys(path);
   const lastIndex = keys.length - 1;
-  keys.reduce((parent, key, index) => {
-    const arrayKey = matchArrayKey(key);
 
-    if (arrayKey) {
+  keys.reduce((parent, key, index) => {
+    const { matched, result: arrayKey } = matchArrayKey(key);
+
+    if (matched && arrayKey) {
       if (!parent[arrayKey]) {
         Vue.set(parent, arrayKey, []);
       }
@@ -75,8 +101,8 @@ export const $delete = (obj: { [key: string]: any }, path: string) => {
       return undefined;
     }
 
-    const arrayKey = matchArrayKey(key);
-    if (arrayKey) {
+    const { matched, result: arrayKey } = matchArrayKey(key);
+    if (matched && arrayKey) {
       return parent[arrayKey];
     }
 
